4dc50e19b7f93af6540f9116cefe00d982e25891
misirlou-tg <39652424+misirlou-tg@users.noreply.github.com>
2025-03-30 15:29:22 -0500

    Changing run-dashboard.sh to run version 9.1.0

--- run-dashboard.sh
+++ run-dashboard.sh
@@ -2,4 +2,4 @@
 # OLTP/gRPC will be on port 4317
 # OLTP/HTTP (port 18890) not mapped
 docker run --rm -it -p 8080:18888 -p 4317:18889 --name aspire-dashboard \
-    mcr.microsoft.com/dotnet/aspire-dashboard:8.1.0
+    mcr.microsoft.com/dotnet/aspire-dashboard:9.1.0
